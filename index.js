c4d123e06fc2c1adde652d0dedeb8054442b0cc1
Chris Pang <chris_pang429@outlook.com>
2017-11-03 06:38:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var name = "Susan"
 var height = "74"
 
-if (name==="Susan") {
-  console.log("The name is correct")
+if (name==="Susan" && height=="74") {
+  console.log("Both the name and height are correct")
 } else {
-  console.log("Expected the name to be Susan")
+  console.log("Expected the name to be Susan and expected height to be 74")
 }
 
 
